8957fe888bb72c61ac0003bbfee45acb917d1910
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-12-11 17:25:39 +0000

    fix: run terraform plan on PR build

--- main.tf
+++ main.tf
@@ -22,10 +22,10 @@ resource "local_file" "jx-requirements" {
     lts_bucket = module.jx.lts_bucket_url
     backup_bucket = module.jx.backup_bucket_url
     vault_bucket = module.jx.vault_bucket_name
-    vault_key =  ""
-    vault_keyring = ""
-    vault_name = ""
-    vault_sa = ""
+    vault_key = module.jx.vault_key
+    vault_keyring = module.jx.vault_keyring
+    vault_name = module.jx.vault_name
+    vault_sa = module.jx.vault_sa
   })
   filename = "${path.module}/jx-requirements.yaml"
 }
