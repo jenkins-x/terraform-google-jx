79b97c376fae40883ffce75e7342b666e7e46410
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-12-11 17:45:20 +0000

    feat: templating vault parameters

--- jx/modules/vault/outputs.tf
+++ jx/modules/vault/outputs.tf
@@ -10,3 +10,14 @@ output "vault_bucket_url" {
   value = "${google_storage_bucket.vault_bucket.url}"
 }
 
+output "vault_key" {
+  value = "${google_kms_crypto_key.vault_crypto_key.name}"
+}
+
+output "vault_keyring" {
+  value = "${google_kms_key_ring.vault_keyring.name}"
+}
+
+output "vault_name" {
+  value = "${var.cluster_name}"
+}
--- jx/output.tf
+++ jx/output.tf
@@ -21,3 +21,19 @@ output "backup_bucket_url" {
 output "vault_bucket_name" {
   value = module.vault.vault_bucket_name
 }
+
+output "vault_key" {
+  value = module.vault.vault_key
+}
+
+output "vault_keyring" {
+  value = module.vault.vault_keyring
+}
+
+output "vault_name" {
+  value = module.vault.vault_name
+}
+
+output "vault_sa" {
+  value = module.vault.vault_sa
+}
