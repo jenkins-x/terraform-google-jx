47e9734e802ee552dc0da3b4161a772b207615fc
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-12-11 17:32:32 +0000

    chore: fmt

--- main.tf
+++ main.tf
@@ -11,21 +11,21 @@ module "jx" {
   cluster_name  = "test-cluster"
   parent_domain = "test.com"
 
-  
+
 }
 
 resource "local_file" "jx-requirements" {
   content = templatefile("${path.module}/jx-requirements.yaml.tpl", {
-    cluster_name = module.jx.cluster_name
-    gcp_project  = module.jx.gcp_project
-    zone  = module.jx.zone
-    lts_bucket = module.jx.lts_bucket_url
+    cluster_name  = module.jx.cluster_name
+    gcp_project   = module.jx.gcp_project
+    zone          = module.jx.zone
+    lts_bucket    = module.jx.lts_bucket_url
     backup_bucket = module.jx.backup_bucket_url
-    vault_bucket = module.jx.vault_bucket_name
-    vault_key = module.jx.vault_key
+    vault_bucket  = module.jx.vault_bucket_name
+    vault_key     = module.jx.vault_key
     vault_keyring = module.jx.vault_keyring
-    vault_name = module.jx.vault_name
-    vault_sa = module.jx.vault_sa
+    vault_name    = module.jx.vault_name
+    vault_sa      = module.jx.vault_sa
   })
   filename = "${path.module}/jx-requirements.yaml"
 }
