628b5591dcf3525dd5ecd2d37976d3c4d6059467
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-12-16 13:46:46 +0000

    chore: add logging into build

--- ci.sh
+++ ci.sh
@@ -7,4 +7,20 @@ echo "Generating Plan..."
 PLAN=$(terraform plan -no-color)
 
 echo "Logging Plan..."
-jx step pr comment --comment="\`\`\`\r${PLAN}"
+jx step pr comment --code --comment="${PLAN}"
+
+PROJECT_NAME=terraform-${BRANCH_NAME}-${BUILD_NUMBER}
+PROJECT_NAME=$( echo $PROJECT_NAME | tr  '[:upper:]' '[:lower:]')
+FOLDER=xxx
+
+echo "Creating project ${PROJECT_NAME} under folder ${FOLDER}..."
+
+echo "Enabling API(s)..."
+
+echo "Applying Terraform..."
+
+echo "Test???"
+
+echo "Cleanup..."
+
+echo "Delete Project..."
