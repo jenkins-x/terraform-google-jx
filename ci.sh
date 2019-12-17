f463c5f9f788a0406d4a85d65a586ff103080f7f
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-12-17 12:33:18 +0000

    chore: adding a trap around the cleanup

--- ci.sh
+++ ci.sh
@@ -8,6 +8,14 @@ CLUSTER_NAME=tf-${BRANCH_NAME}-${BUILD_NUMBER}
 CLUSTER_NAME=$( echo ${CLUSTER_NAME} | tr  '[:upper:]' '[:lower:]')
 VARS="-var gcp_project=terraform-test -var region=europe-west1 -var zone=europe-west1-b -var cluster_name=${CLUSTER_NAME}"
 
+function cleanup()
+{
+	echo "Cleanup..."
+	terraform destroy $VARS -auto-approve
+}
+
+trap cleanup EXIT
+
 echo "Generating Plan..."
 PLAN=$(terraform plan $VARS -no-color)
 
@@ -19,7 +27,3 @@ echo "Creating cluster ${CLUSTER_NAME} in project ${PROJECT}..."
 echo "Applying Terraform..."
 terraform apply $VARS -auto-approve
 
-echo "Test???"
-
-echo "Cleanup..."
-terraform destroy $VARS -auto-approve
