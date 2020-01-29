91fc84188d2fbbfe28bb51920c51783e86fe51ec
Cai Cooper <caicooper82@gmail.com>
2020-01-29 17:08:53 +0000

    fix: pass in the parent_domain
    Signed-off-by: Cai Cooper <caicooper82@gmail.com>

--- ci.sh
+++ ci.sh
@@ -10,7 +10,8 @@ PROJECT=terraform-test-261120
 #PROJECT=terraform-test
 CLUSTER_NAME=tf-${BRANCH_NAME}-${BUILD_NUMBER}
 CLUSTER_NAME=$( echo ${CLUSTER_NAME} | tr  '[:upper:]' '[:lower:]')
-VARS="-var gcp_project=${PROJECT} -var region=europe-west1 -var zone=europe-west1-b -var cluster_name=${CLUSTER_NAME}"
+PARENT_DOMAIN="${CLUSTER_NAME}.jenkins-x-test.test"
+VARS="-var gcp_project=${PROJECT} -var region=europe-west1 -var zone=europe-west1-b -var cluster_name=${CLUSTER_NAME} -var parent_domain=${PARENT_DOMAIN}"
 
 function cleanup()
 {
