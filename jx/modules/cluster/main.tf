c6e505f92e2ac051e525b56f52583f46c7a676f0
Cai Cooper <caicooper82@gmail.com>
2020-01-30 13:32:39 +0000

    feat: added autoscaling node pool
    Signed-off-by: Cai Cooper <caicooper82@gmail.com>

--- jx/modules/cluster/main.tf
+++ jx/modules/cluster/main.tf
@@ -1,29 +1,15 @@
 resource "google_container_cluster" "jx_cluster" {
-  provider                = "google-beta"
-  name                    = var.cluster_name
-  description             = "jx k8s cluster"
-  location                = var.zone
-  enable_kubernetes_alpha = var.enable_kubernetes_alpha
-  enable_legacy_abac      = var.enable_legacy_abac
-  initial_node_count      = var.min_node_count
-  logging_service         = var.logging_service
-  monitoring_service      = var.monitoring_service
+  provider                 = "google-beta"
+  name                     = var.cluster_name
+  description              = "jx k8s cluster"
+  location                 = var.zone
+  enable_kubernetes_alpha  = var.enable_kubernetes_alpha
+  enable_legacy_abac       = var.enable_legacy_abac
+  initial_node_count       = var.min_node_count
+  logging_service          = var.logging_service
+  monitoring_service       = var.monitoring_service
+  remove_default_node_pool = "true"
 
-  node_config {
-    preemptible  = var.node_preemptible
-    machine_type = var.node_machine_type
-    disk_size_gb = var.node_disk_size
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.full_control",
-      "https://www.googleapis.com/auth/service.management",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
   resource_labels = {
     type = var.test_cluster_label
   }
@@ -41,6 +27,44 @@ resource "google_container_cluster" "jx_cluster" {
   //lifecycle {
   //  prevent_destroy = true
   //}
+}
+
+resource "google_container_node_pool" "jx_node_pool" {
+  provider                = "google-beta"
+  name                    = "autoscale-pool"
+  location                = var.zone
+  cluster                 = google_container_cluster.jx_cluster.name
+  node_count              = var.min_node_count
+
+  node_config {
+    preemptible  = var.node_preemptible
+    machine_type = var.node_machine_type
+    disk_size_gb = var.node_disk_size
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.full_control",
+      "https://www.googleapis.com/auth/service.management",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+
+  autoscaling {
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
+  }
+
+  management {
+    auto_repair  = "true"
+    auto_upgrade = "false"
+  }
+
+  depends_on = [
+    google_container_cluster.jx_cluster
+  ]
 }
 
 resource "google_storage_bucket" "lts_bucket" {
