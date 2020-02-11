de48b8f6ff30228df988d2647cbde0e881501b84
Cai Cooper <caicooper82@gmail.com>
2020-02-11 14:39:43 +0000

    fix: add service management read oauth scope to the node pool
    Signed-off-by: Cai Cooper <caicooper82@gmail.com>

--- jx/modules/cluster/main.tf
+++ jx/modules/cluster/main.tf
@@ -43,6 +43,7 @@ resource "google_container_node_pool" "jx_node_pool" {
       "https://www.googleapis.com/auth/devstorage.full_control",
       "https://www.googleapis.com/auth/service.management",
       "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
