f0e0925182e684c9ed15dd15e437225eb399b8d6
Hardy Ferentschik <hardy@ferentschik.de>
2020-03-09 16:50:06 +0100

    chore: updating Jenkins X pipeline config

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 set -u
@@ -7,30 +7,42 @@ echo $GOOGLE_APPLICATION_CREDENTIALS
 cat $GOOGLE_APPLICATION_CREDENTIALS
 
 PROJECT=terraform-test-261120
-#PROJECT=terraform-test
 CLUSTER_NAME=tf-${BRANCH_NAME}-${BUILD_NUMBER}
 CLUSTER_NAME=$( echo ${CLUSTER_NAME} | tr  '[:upper:]' '[:lower:]')
 PARENT_DOMAIN="${CLUSTER_NAME}.jenkins-x-test.test"
-VARS="-var gcp_project=${PROJECT} -var region=europe-west1 -var zone=europe-west1-b -var cluster_name=${CLUSTER_NAME} -var parent_domain=${PARENT_DOMAIN}"
 
 function cleanup()
 {
 	echo "Cleanup..."
-	terraform destroy $VARS -auto-approve
+	make destroy
 }
 
 trap cleanup EXIT
 
-echo "Generating Plan..."
-PLAN=$(terraform plan $VARS -no-color)
-
-echo "Logging Plan..."
-jx step pr comment --code --comment="${PLAN}"
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+gcloud auth list
+gcloud config set project $PROJECT
 
 echo "Creating cluster ${CLUSTER_NAME} in project ${PROJECT}..."
+echo "gcp_project   = \"${PROJECT}\"" >> terraform.tfvars
+echo "zone          = \"europe-west1-b\"" >> terraform.tfvars
+echo "cluster_name  = \"${CLUSTER_NAME}\"" >> terraform.tfvars
+echo "parent_domain = \"${PARENT_DOMAIN}\"" >> terraform.tfvars
+echo "force_destroy = true" >> terraform.tfvars
+echo "" >> terraform.tfvars
+make plan
+make apply
 
-echo "Applying Terraform..."
-terraform apply $VARS -auto-approve
+echo "Logging generated jx-requirements.yaml..."
+cat jx-requirements.yaml
 
-JX_REQUIREMENTS=$(cat jx-requirements.yaml)
-jx step pr comment --code --comment="${JX_REQUIREMENTS}"
+echo "Installing shellspec"
+pushd /var/tmp
+git clone https://github.com/shellspec/shellspec.git
+export PATH=/var/tmp/shellspec/bin:${PATH}
+popd
+
+echo "Running shellspec tests..."
+# waiting some time to let verything propagate, otherwise tests like workload identity verification can fail
+sleep 60 
+make test
