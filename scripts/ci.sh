a74ad7478cc9e7729a2d1a74a7bd28714bb7561d
Hardy Ferentschik <hardy@ferentschik.de>
2020-03-09 16:50:06 +0100

    chore: updating Jenkins X pipeline config

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 set -u
@@ -7,11 +7,10 @@ echo $GOOGLE_APPLICATION_CREDENTIALS
 cat $GOOGLE_APPLICATION_CREDENTIALS
 
 PROJECT=terraform-test-261120
-#PROJECT=terraform-test
 CLUSTER_NAME=tf-${BRANCH_NAME}-${BUILD_NUMBER}
 CLUSTER_NAME=$( echo ${CLUSTER_NAME} | tr  '[:upper:]' '[:lower:]')
 PARENT_DOMAIN="${CLUSTER_NAME}.jenkins-x-test.test"
-VARS="-var gcp_project=${PROJECT} -var region=europe-west1 -var zone=europe-west1-b -var cluster_name=${CLUSTER_NAME} -var parent_domain=${PARENT_DOMAIN}"
+VARS="-var gcp_project=${PROJECT} -var zone=europe-west1-b -var cluster_name=${CLUSTER_NAME} -var parent_domain=${PARENT_DOMAIN}"
 
 function cleanup()
 {
@@ -22,15 +21,21 @@ function cleanup()
 trap cleanup EXIT
 
 echo "Generating Plan..."
-PLAN=$(terraform plan $VARS -no-color)
-
-echo "Logging Plan..."
-jx step pr comment --code --comment="${PLAN}"
+terraform plan $VARS -no-color
 
 echo "Creating cluster ${CLUSTER_NAME} in project ${PROJECT}..."
-
-echo "Applying Terraform..."
 terraform apply $VARS -auto-approve
 
-JX_REQUIREMENTS=$(cat jx-requirements.yaml)
-jx step pr comment --code --comment="${JX_REQUIREMENTS}"
+echo "Logging generated jx-requirements.yaml..."
+cat jx-requirements.yaml
+
+echo "Installing shellspec"
+pushd /var/tmp
+git clone https://github.com/shellspec/shellspec.git
+export PATH=/var/tmp/shellspec/bin:${PATH}
+popd
+
+echo "Running shellspec tests..."
+# waiting some time to let verything propagate, otherwise tests like workload identity verification can fail
+sleep 60 
+make test
