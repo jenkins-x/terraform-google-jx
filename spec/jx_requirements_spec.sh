91d4b9030fe1d1625e319e9cc27d6d58df0f0e5d
Cai Cooper <caicooper82@gmail.com>
2020-05-06 15:20:51 +0100

    chore: updated spec test to use new req filename

--- spec/jx_requirements_spec.sh
+++ spec/jx_requirements_spec.sh
@@ -2,12 +2,12 @@
 
 Describe 'jx-requirements'
   It 'jx-requirements contains cluster name'
-    When call yq r jx-requirements.yml 'cluster.clusterName'
+    When call yq r ${terraform output gcp_project}-jx-requirements.yml 'cluster.clusterName'
     The output should eq $(terraform output cluster_name)
   End 
 
   It 'jx-requirements contains ingress section'
-    When call yq r jx-requirements.yml 'ingress'
+    When call yq r ${terraform output gcp_project}-jx-requirements.yml 'ingress'
     The output should start with "domain"
   End 
 End
