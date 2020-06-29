b29684cdeb8ce6f592b7508f6871a6cc8c3c34cc
Hardy Ferentschik <hardy@ferentschik.de>
2020-06-29 10:48:23 +0200

    feat: adding GCP and Kubernetes service accounts for JX UI
    fixes #52

--- spec/k8s_spec.sh
+++ spec/k8s_spec.sh
@@ -50,7 +50,14 @@ Describe "Kubernetes"
     It "Service account velero-server has workload identity annotation"
       When call service_account_get_annotation velero-server velero
       The output should eq "$(terraform output cluster_name)-vo@$(terraform output gcp_project).iam.gserviceaccount.com"
-    End     
+    End  
+
+    # UI
+    It "Service account jxui has workload identity annotation"
+      When call service_account_get_annotation jxui-sa jx
+      The output should eq "$(terraform output cluster_name)-jxui@$(terraform output gcp_project).iam.gserviceaccount.com"
+    End
+
   End
 
   Describe "Workload Identity"
@@ -99,7 +106,12 @@ Describe "Kubernetes"
     It "Pod with exdns-external-dns service account uses workload identity"
       When call workload_identity_test exdns-external-dns jx
       The output should include "$(terraform output cluster_name)-dn@$(terraform output gcp_project).iam.gserviceaccount.com"
-    End        
+    End    
+
+    It "Pod with jxui service account uses workload identity"
+      When call workload_identity_test jxui-sa jx
+      The output should include "$(terraform output cluster_name)-jxui@$(terraform output gcp_project).iam.gserviceaccount.com"
+    End          
   End
 
   Describe "Cluster"
