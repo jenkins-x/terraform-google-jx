88bee2f8062791b43a45f8bd37b3e37e5b5d01a4
ankitm123 <ankitmohapatra123@gmail.com>
2020-08-01 09:07:49 -0400

    fix: check if tuples are empty
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>

--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -1,31 +1,31 @@
 output "cluster_name" {
-    value = google_container_cluster.jx_cluster.name
+  value = google_container_cluster.jx_cluster.name
 }
 
 output "cluster_location" {
-    value = google_container_cluster.jx_cluster.location
+  value = google_container_cluster.jx_cluster.location
 }
 
 output "cluster_endpoint" {
-    value = google_container_cluster.jx_cluster.endpoint
+  value = google_container_cluster.jx_cluster.endpoint
 }
 
 output "cluster_ca_certificate" {
-    value = google_container_cluster.jx_cluster.master_auth[0].cluster_ca_certificate
+  value = length(google_container_cluster.jx_cluster.master_auth) > 0 ? google_container_cluster.jx_cluster.master_auth[0].cluster_ca_certificate : ""
 }
 
 output "log_storage_url" {
-    value = google_storage_bucket.log_bucket[0].url
+  value = length(google_storage_bucket.log_bucket) > 0 ? google_storage_bucket.log_bucket[0].url : ""
 }
 
 output "report_storage_url" {
-    value = google_storage_bucket.report_bucket[0].url
+  value = length(google_storage_bucket.report_bucket) > 0 ? google_storage_bucket.report_bucket[0].url : ""
 }
 
 output "repository_storage_url" {
-    value = google_storage_bucket.repository_bucket[0].url
+  value = length(google_storage_bucket.repository_bucket) > 0 ? google_storage_bucket.repository_bucket[0].url : ""
 }
 
 output "jenkins_x_namespace" {
-    value = kubernetes_namespace.jenkins_x_namespace.metadata[0].name
+  value = length(kubernetes_namespace.jenkins_x_namespace.metadata) > 0 ? kubernetes_namespace.jenkins_x_namespace.metadata[0].name : ""
 }
--- modules/vault/main.tf
+++ modules/vault/main.tf
@@ -1,5 +1,7 @@
 locals {
-  sa_name = "${var.cluster_name}-vt"
+  sa_name        = "${var.cluster_name}-vt"
+  vault_sa_name  = length(google_service_account.vault_sa) > 0 ? google_service_account.vault_sa[0].name : ""
+  vault_sa_email = length(google_service_account.vault_sa) > 0 ? google_service_account.vault_sa[0].email : ""
 }
 
 // ----------------------------------------------------------------------------
@@ -8,7 +10,7 @@ locals {
 // https://www.terraform.io/docs/providers/google/r/google_project_service.html
 // ----------------------------------------------------------------------------
 resource "google_project_service" "cloudkms_api" {
-  count              = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
   provider           = google
   project            = var.gcp_project
@@ -22,22 +24,22 @@ resource "google_project_service" "cloudkms_api" {
 // https://www.terraform.io/docs/providers/google/d/google_kms_key_ring.html
 // ---------------------------------------------------------------------------
 resource "google_kms_key_ring" "vault_keyring" {
-  count       = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
-  provider    = google
-  name        = "keyring-${var.cluster_name}-${var.cluster_id}"
-  location    = "global"
-  depends_on  = [google_project_service.cloudkms_api]
+  provider   = google
+  name       = "keyring-${var.cluster_name}-${var.cluster_id}"
+  location   = "global"
+  depends_on = [google_project_service.cloudkms_api]
 }
 
 resource "google_kms_crypto_key" "vault_crypto_key" {
-  count            = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
-  provider         = google
-  name             = "crypto-key-${var.cluster_name}-${var.cluster_id}"
-  key_ring         = google_kms_key_ring.vault_keyring[0].self_link
-  rotation_period  = "100000s"
-  depends_on       = [google_project_service.cloudkms_api]
+  provider        = google
+  name            = "crypto-key-${var.cluster_name}-${var.cluster_id}"
+  key_ring        = google_kms_key_ring.vault_keyring[0].self_link
+  rotation_period = "100000s"
+  depends_on      = [google_project_service.cloudkms_api]
 }
 
 // ----------------------------------------------------------------------------
@@ -46,7 +48,7 @@ resource "google_kms_crypto_key" "vault_crypto_key" {
 // https://www.terraform.io/docs/providers/google/r/storage_bucket.html
 // ----------------------------------------------------------------------------
 resource "google_storage_bucket" "vault_bucket" {
-  count         = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
   provider      = google
   name          = "vault-${var.cluster_name}-${var.cluster_id}"
@@ -61,35 +63,35 @@ resource "google_storage_bucket" "vault_bucket" {
 // https://www.terraform.io/docs/providers/google/r/google_project_iam.html#google_project_iam_member
 // ----------------------------------------------------------------------------
 resource "google_service_account" "vault_sa" {
-  count        = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
   provider     = google
   account_id   = local.sa_name
-  display_name = substr("Vault service account for cluster ${var.cluster_name}", 0, 100)          
+  display_name = substr("Vault service account for cluster ${var.cluster_name}", 0, 100)
 }
 
 resource "google_project_iam_member" "vault_sa_storage_object_admin_binding" {
-  count    = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
   provider = google
   role     = "roles/storage.objectAdmin"
-  member   = "serviceAccount:${google_service_account.vault_sa[0].email}"
+  member   = "serviceAccount:${local.vault_sa_email}"
 }
 
 resource "google_project_iam_member" "vault_sa_cloudkms_admin_binding" {
-  count    = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
   provider = google
   role     = "roles/cloudkms.admin"
-  member   = "serviceAccount:${google_service_account.vault_sa[0].email}"
+  member   = "serviceAccount:${local.vault_sa_email}"
 }
 
 resource "google_project_iam_member" "vault_sa_cloudkms_crypto_binding" {
-  count    = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
   provider = google
   role     = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member   = "serviceAccount:${google_service_account.vault_sa[0].email}"
+  member   = "serviceAccount:${local.vault_sa_email}"
 }
 
 // ----------------------------------------------------------------------------
@@ -99,19 +101,19 @@ resource "google_project_iam_member" "vault_sa_cloudkms_crypto_binding" {
 // https://www.terraform.io/docs/providers/kubernetes/r/service_account.html
 // ----------------------------------------------------------------------------
 resource "google_service_account_iam_member" "vault_auth_workload_identity_user" {
-  count              = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
   provider           = google
-  service_account_id = google_service_account.vault_sa[0].name
+  service_account_id = local.vault_sa_name
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.gcp_project}.svc.id.goog[${var.jenkins_x_namespace}/vault-auth]"
 }
 
 resource "google_service_account_iam_member" "vault_operator_workload_identity_user" {
-  count              = var.external_vault ? 0 : 1
+  count = var.external_vault ? 0 : 1
 
   provider           = google
-  service_account_id = google_service_account.vault_sa[0].name
+  service_account_id = local.vault_sa_name
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.gcp_project}.svc.id.goog[${var.jenkins_x_namespace}/${var.cluster_name}-vt]"
 }
@@ -119,10 +121,10 @@ resource "google_service_account_iam_member" "vault_operator_workload_identity_u
 resource "kubernetes_service_account" "vault_sa" {
   automount_service_account_token = true
   metadata {
-    name = local.sa_name
+    name      = local.sa_name
     namespace = var.jenkins_x_namespace
     annotations = var.external_vault ? {} : {
-      "iam.gke.io/gcp-service-account" = google_service_account.vault_sa[0].email
+      "iam.gke.io/gcp-service-account" = local.vault_sa_email
     }
   }
   lifecycle {
