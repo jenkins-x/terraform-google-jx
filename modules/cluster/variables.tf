11c3546b27d8dd8eb153ec0c1024f63f9e4e8f02
James Rawlings <rawlingsj80@gmail.com>
2020-09-08 13:16:15 +0100

    feat: jx3 lets install the Jenins X git operator to pass management of the installed applications to Jenkins X rather than Terraform

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -186,3 +186,21 @@ variable "content" {
   type        = string
   default     = ""
 }
+
+variable "jx_git_url" {
+  description = "URL for the Jenins X cluster git repository"
+  type        = string
+  default     = ""
+}
+
+variable "jx_bot_username" {
+  description = "Bot username used to interact with the Jenkins X cluster git repository"
+  type        = string
+  default     = ""
+}
+
+variable "jx_bot_token" {
+  description = "Bot token used to interact with the Jenkins X cluster git repository"
+  type        = string
+  default     = ""
+}
