fa59454f082094edff0262721149212e14757ca0
James Rawlings <rawlingsj80@gmail.com>
2020-11-05 00:05:25 +0000

    feat: jx3 install health components once cluster has been created

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -76,6 +76,15 @@ resource "google_container_cluster" "jx_cluster" {
   }
 }
 
+module "jx-health" {
+  count  = var.jx2 ? 0 : 1
+  source = "github.com/jenkins-x/terraform-jx-health?ref=main"
+
+  depends_on = [
+    google_container_cluster.jx_cluster
+  ]
+}
+
 // ----------------------------------------------------------------------------
 // Add main Jenkins X Kubernetes namespace
 // 
