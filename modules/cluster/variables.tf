65da642fa2d656414dd286cc5776b20be621617a
amit.kumar <amit.kumar@galaxycard.in>
2020-11-18 15:14:50 +0530

    Revert changes to instance types

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -75,30 +75,28 @@ variable "node_machine_type" {
 variable "machine_types_cpu" {
   type = map
   default = {
-    "n1-standard-1"    = 1
-    "n1-standard-2"    = 2
-    "n1-standard-4"    = 4
-    "n1-standard-8"    = 8
-    "n1-standard-16"   = 16
-    "n1-standard-32"   = 32
-    "n1-standard-64"   = 64
-    "n1-standard-96"   = 96
-    "e2-custom-2-4096" = 2
+    "n1-standard-1"  = 1
+    "n1-standard-2"  = 2
+    "n1-standard-4"  = 4
+    "n1-standard-8"  = 8
+    "n1-standard-16" = 16
+    "n1-standard-32" = 32
+    "n1-standard-64" = 64
+    "n1-standard-96" = 96
   }
 }
 
 variable "machine_types_memory" {
   type = map
   default = {
-    "n1-standard-1"    = 3.75
-    "n1-standard-2"    = 7.50
-    "n1-standard-4"    = 15
-    "n1-standard-8"    = 30
-    "n1-standard-16"   = 60
-    "n1-standard-32"   = 120
-    "n1-standard-64"   = 240
-    "n1-standard-96"   = 360
-    "e2-custom-2-4096" = 4
+    "n1-standard-1"  = 3.75
+    "n1-standard-2"  = 7.50
+    "n1-standard-4"  = 15
+    "n1-standard-8"  = 30
+    "n1-standard-16" = 60
+    "n1-standard-32" = 120
+    "n1-standard-64" = 240
+    "n1-standard-96" = 360
   }
 }
 
@@ -191,7 +189,7 @@ variable "create_ui_sa" {
 variable "jx2" {
   description = "Is a Jenkins X 2 install"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "content" {
