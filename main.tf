09da8d82fc2aa483cb62393ebc82e2d0c6e32b0e
ankitm123 <ankitmohapatra123@gmail.com>
2022-05-12 10:43:06 -0400

    fix: remove versions from provider block
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@
 // ----------------------------------------------------------------------------
 terraform {
   required_version = ">= 0.12.0, < 2.0"
+  required_providers {
+    google      = ">= 3.46.0, < 4.0.0"
+    google-beta = ">= 3.46.0, < 4.0.0"
+    random      = ">= 2.2.0"
+    local       = ">= 1.2.0"
+    null        = ">= 2.1.0"
+    template    = ">= 2.1.0"
+  }
 }
 
 // ----------------------------------------------------------------------------
@@ -12,28 +20,22 @@ terraform {
 // ----------------------------------------------------------------------------
 provider "google" {
   project = var.gcp_project
-  version = ">= 3.46.0, < 4.0.0"
 }
 
 provider "google-beta" {
   project = var.gcp_project
-  version = ">= 3.46.0, < 4.0.0"
 }
 
 provider "random" {
-  version = ">= 2.2.0"
 }
 
 provider "local" {
-  version = ">= 1.2.0"
 }
 
 provider "null" {
-  version = ">= 2.1.0"
 }
 
 provider "template" {
-  version = ">= 2.1.0"
 }
 
 data "google_client_config" "default" {
@@ -173,9 +175,9 @@ module "cluster" {
   jx2          = var.jx2
   content      = local.content
 
-  jx_git_url      = var.jx_git_url
-  jx_bot_username = var.jx_bot_username
-  jx_bot_token    = var.jx_bot_token
+  jx_git_url              = var.jx_git_url
+  jx_bot_username         = var.jx_bot_username
+  jx_bot_token            = var.jx_bot_token
   jx_git_operator_version = var.jx_git_operator_version
 
   kuberhealthy = var.kuberhealthy
@@ -186,7 +188,7 @@ module "cluster" {
 // See https://github.com/banzaicloud/bank-vaults
 // ----------------------------------------------------------------------------
 module "vault" {
-  count  = ! var.gsm ? 1 : 0
+  count  = !var.gsm ? 1 : 0
   source = "./modules/vault"
 
   gcp_project         = var.gcp_project
@@ -204,7 +206,7 @@ module "vault" {
 // See https://cloud.google.com/secret-manager
 // ----------------------------------------------------------------------------
 module "gsm" {
-  count  = var.gsm && ! var.jx2 ? 1 : 0
+  count  = var.gsm && !var.jx2 ? 1 : 0
   source = "./modules/gsm"
 
   gcp_project  = var.gcp_project
@@ -257,7 +259,7 @@ module "dns" {
 module "jx-boot" {
   source        = "./modules/jx-boot"
   depends_on    = [module.cluster]
-  install_vault = ! var.gsm ? true : false
+  install_vault = !var.gsm ? true : false
 }
 
 // ----------------------------------------------------------------------------
@@ -285,7 +287,7 @@ locals {
     vault_name      = length(module.vault) > 0 ? module.vault[0].vault_name : ""
     vault_sa        = length(module.vault) > 0 ? module.vault[0].vault_sa : ""
     vault_url       = var.vault_url
-    vault_installed = ! var.gsm ? true : false
+    vault_installed = !var.gsm ? true : false
     // Velero
     enable_backup    = var.enable_backup
     velero_sa        = module.backup.velero_sa
